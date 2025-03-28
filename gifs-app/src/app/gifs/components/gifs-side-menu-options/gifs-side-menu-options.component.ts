388e7b5254fdce092d73da9b7e923347f4fb046c
Lurgia Yupa Arias <lyupa@sintad.pe>
2025-03-27 21:11:29 -0500

    feat: menu options

--- gifs-app/src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ gifs-app/src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
   styleUrl: './gifs-side-menu-options.component.css'
 })
 export class GifsSideMenuOptionsComponent {
 
+  menuOption: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs populares'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      subLabel: 'Buscar gifs'
+    },
+
+  ];
+}
+
+export interface MenuOptions {
+  route: string;
+  icon: string;
+  label?: string;
+  subLabel: string;
 }
